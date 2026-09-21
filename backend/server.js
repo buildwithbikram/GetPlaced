27e99bacbbf01c7ce4ba630ad8dbab218f09e7ef
buildwithbikram <bikrambakshi07@gmail.com>
2026-09-21 19:19:49 +0530

    "feat: connect GetPlaced backend to MongoDB"

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
+require('dotenv').config();
+
 const express = require('express');
 const cors = require('cors');
 
+const connectDB = require('./config/db');
 
 const PORT = 5000;
 
@@ -9,6 +12,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+connectDB();
 
 app.get('/', (req, res) => {
     res.send("GetPlaced Backend is running!");
