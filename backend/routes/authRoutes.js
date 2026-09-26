ea331cf43b64edb3e2938c95ea6a4778aabb3428
buildwithbikram <bikrambakshi07@gmail.com>
2026-09-26 22:37:24 +0530

    "feat: add role-based authorization middleware"

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,5 +1,8 @@
 const express = require('express');
 
+const { protect } = require('../middleware/authMiddleware');
+const { authorizeRoles } = require('../middleware/roleMiddleware');
+
 const {registerUser, loginUser, getMe} = require('../controllers/authController');
 
 const router = express.Router();
@@ -8,5 +11,16 @@ router.post('/register', registerUser);
 router.post('/login', loginUser);
 
 router.get('/me', getMe);
+router.get(
+  '/admin-test',
+  protect,
+  authorizeRoles('admin'),
+  (req, res) => {
+    res.status(200).json({
+      success: true,
+      message: 'Welcome, Admin!'
+    });
+  }
+);
 
 module.exports = router;
